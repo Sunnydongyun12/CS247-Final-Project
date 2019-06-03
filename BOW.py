af7cefaaac005597800f0a4ac8edf271c4c55050
Sunnydongyun12 <sunnydongyun@gmail.com>
2019-06-03 12:47:19 -0700

    data file name revise

--- BOW.py
+++ BOW.py
@@ -66,40 +66,37 @@ def bow(X_train, X_test, y_train, y_test):
         for w in words:
             w = stemmer.stem(w)
             temp.append(w)
-        X_train_stem.append(" ".join(temp))
+        X_test_stem.append(" ".join(temp))
     print('stemming success!!!')
 
     vectorizer, tfidf_transformer = import_model()
+    # print(vectorizer.get_feature_names())
 
-    X_train_stem = X_train
-    X_test_stem = X_test
     print('vectorize data...')
 
     # vectorizer = CountVectorizer(stop_words='english', token_pattern=r'\b[^\d\W_]+\b', min_df=3)
     # training_vectors = vectorizer.fit_transform(X_train_stem)
-
     training_vectors = vectorizer.transform(X_train_stem)
     testing_vectors = vectorizer.transform(X_test_stem)
     # print sample vocabulary
     # vectorizer.get_feature_names()
-    print('vectorize success!!!')
+    # print('vectorize success!!!')
 
     print('transform data into tfidf matrix...')
     tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(training_vectors)
-    X_test_tfidf = tfidf_transformer.transform(testing_vectors)
-    print('the final tfidf train matrix with shape {}'.format(X_train_tfidf.shape))
-    print('the final tfidf test matrix with shape {}'.format(X_test_tfidf.shape))
+    X_train_bow_rest = tfidf_transformer.fit_transform(training_vectors)
+    X_test_bow_rest = tfidf_transformer.transform(testing_vectors)
+    print('the final tfidf train matrix with shape {}'.format(X_train_bow_rest.shape))
+    print('the final tfidf test matrix with shape {}'.format(X_test_bow_rest.shape))
     print('yeah! done!')
 
-    with open('data/X_train_tfidf_rest', 'wb') as fp:
-        pickle.dump(X_train_tfidf, fp)
-    with open('data/X_test_tfidf_rest', 'wb') as fp:
-        pickle.dump(X_test_tfidf, fp)
-    with open('data/y_train_rest', 'wb') as fp:
+    with open('data/X_train_bow_rest', 'wb') as fp:
+        pickle.dump(X_train_bow_rest, fp)
+    with open('data/X_test_bow_rest', 'wb') as fp:
+        pickle.dump(X_test_bow_rest, fp)
+    with open('data/y_train_bow_rest', 'wb') as fp:
         pickle.dump(y_train, fp)
-    with open('data/y_test_rest', 'wb') as fp:
-
+    with open('data/y_test_bow_rest', 'wb') as fp:
         pickle.dump(y_test, fp)
     print('The test and training data are dumped in X_train_tfidf, X_test_tfidf, y_train, y_test')
 
